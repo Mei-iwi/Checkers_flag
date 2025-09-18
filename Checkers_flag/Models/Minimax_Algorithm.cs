27e4e2b0c4c1a9ba6b4082fa4be709e49ebeb2f5
siudepzai <nguyenthanhtuan01081996@gmail.com>
2025-09-18 13:16:04 +0700

    update

--- Checkers_flag/Models/Minimax_Algorithm.cs
+++ Checkers_flag/Models/Minimax_Algorithm.cs
@@ -1,63 +1,73 @@
 ﻿namespace Checkers_flag.Models
 {
-    public class Minimax_Algorithm:checkgame
+    public class Minimax_Algorithm : checkgame
     {
         //Code for Minimax Algorithm will go here
-        public static int max(int a, int b)
+        public int dk_dung(int I, int J)
         {
-            if (a > b) return a;
-            return b;
-        }
-        public static int min(int a, int b)
-        {
-            if (a > b) return b;
-            return a;
-        }
-        public int dk_dung( int I, int J)
-        {
-            if(ktr(I, J, 1)) return 1; // AI thắng
-            if(ktr(I, J, 2)) return -1; // Người chơi thắng
+            if (ktr(I, J, 1)) return 1; // AI thắng
+            if (ktr(I, J, 2)) return -1; // Người chơi thắng
             return 0; // Hòa
         }
-        public int minimax(List<int> game_tree,int tang,Boolean AIchoi,int x, int y)
+        public int minimax(List<int> game_tree, int tang, Boolean AIchoi, int x, int y, out int tdi, out int tdj)
         {
             // kiều kiện dừng
 
-            int kq= dk_dung(x, y);
-            if(kq != 0 || tang==3)
-                return 0;
+            int kq = dk_dung(x, y);
+            if (kq != 0 || tang == 2)
+            {
+                tdi = x;
+                tdj = y;
+                return kq;
+            }
+
             //lượt của ai đánh
             if (AIchoi)
             {
-                int ndtn= int.MinValue;
+                int ndtn = int.MinValue;
+                tdi = -1;
+                tdj = -1;
                 for (int i = 0; i < game_tree.Count; i++)
                 {
                     int ndht = game_tree[i];
-                 x = i / 10;
-                 y = i % 10;
-                    int value = minimax(game_tree, tang + 1, false,x,y);
-                    ndtn = max(ndtn, value);
-
+                    x = i / 10;
+                    y = i % 10;
+                    int value = minimax(game_tree, tang + 1, false, x, y, out _, out _);
+                    if (ndtn < value)
+                    {
+                        ndtn = value;
+                        tdi = x;
+                        tdj = y;
+                    }
                 }
                 return ndtn;
+
             }
             else
             {
                 int ndtn = int.MaxValue;
-                for(int i= 0; i<game_tree.Count; i++)
+                tdi = -1;
+                tdj = -1;
+                for (int i = 0; i < game_tree.Count; i++)
                 {
-                    int ndht= game_tree[i];
+                    int ndht = game_tree[i];
                     x = i / 10;
                     y = i % 10;
-                    int value= minimax(game_tree,tang + 1, true,x,y);
-                    ndtn= min(ndtn, value);
+                    int value = minimax(game_tree, tang + 1, true, x, y, out _, out _);
+                    if (ndtn > value)
+                    {
+                        ndtn = value;
+                        tdi = x;
+                        tdj = y;
+                    }
                 }
-                 return ndtn;
+                return ndtn;
             }
         }
     }
-
-
 }
+
+
+
 
 
