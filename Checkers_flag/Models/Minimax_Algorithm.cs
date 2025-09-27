88273df09ccda122b05776a3c88af15681b3267a
siudepzai <nguyenthanhtuan01081996@gmail.com>
2025-09-27 23:45:04 +0700

    update

--- Checkers_flag/Models/Minimax_Algorithm.cs
+++ Checkers_flag/Models/Minimax_Algorithm.cs
@@ -12,59 +12,65 @@
 
         //cách tính điểm cho thuật toán minimax
         
-        public int minimax(List<int> game_tree, int tang, Boolean AIchoi, int x, int y, out int tdi, out int tdj)
+        public int minimax(List<int> game_tree, int tang, Boolean AIchoi, int x, int y,int alpha,int beta, out int tdi, out int tdj) // dùng out_ để trả thêm tọa độ i,j cho nước đi
         {
-            // kiều kiện dừng
-
-            int kq = dk_dung(x, y);
+            // kiểm tra điều kiện dừng
+            int kq = dk_dung(x, y); 
             if (kq != 0 || tang == 2)
             {
                 tdi = x;
                 tdj = y;
-                return kq;
+                return kq; // trả về kết quả nếu có người thắng hoặc đạt độ sâu tối đa
             }
-
+            //gán giá trị ban đầu cho tọa độ i,j
+            tdi = -1;
+            tdj = -1;
             //lượt của ai đánh
-            if (AIchoi)
+            if (AIchoi) // lượt của AI với giá trị true
             {
-                int ndtn = int.MinValue;
-                tdi = -1;
-                tdj = -1;
+                int ndtn = int.MinValue;// gán giá trị nhỏ nhất
+
                 for (int i = 0; i < game_tree.Count; i++)
                 {
-                    int ndht = game_tree[i];
-                    x = i / 10;
-                    y = i % 10;
-                    int value = minimax(game_tree, tang + 1, false, x, y, out _, out _);
-                    if (ndtn < value)
+                    int nx = i / 10; // tính tọa độ x
+                    int ny = i % 10; // tính tọa độ y
+                    int ndht = diemtungnuoc(nx,ny,1); // tính điểm nước đi của AI
+                    int value = minimax(game_tree, tang + 1, false, nx, ny,alpha,beta, out _, out _); // gọi đệ quy
+                    int toi_uu=Math.Max(value, ndht);
+                    if (ndtn < toi_uu) 
                     {
-                        ndtn = value;
-                        tdi = x;
-                        tdj = y;
+                        ndtn = toi_uu;// cập nhật giá trị lớn nhất
+                        tdi = nx; // cập nhật tọa độ i
+                        tdj = ny; // cập nhật tọa độ j
                     }
+                    alpha = Math.Max(alpha, ndtn);
+                    if (beta <= alpha) // cắt tỉa nhánh
+                        break;
                 }
-                return ndtn;
+                return ndtn;// trả về giá trị lớn nhất kèm tọa độ
 
             }
-            else
+            else // lượt của người chơi với giá trị false
             {
-                int ndtn = int.MaxValue;
-                tdi = -1;
-                tdj = -1;
+                int ndtn_dt = int.MaxValue;// gán giá trị lớn nhất
                 for (int i = 0; i < game_tree.Count; i++)
                 {
-                    int ndht = game_tree[i];
-                    x = i / 10;
-                    y = i % 10;
-                    int value = minimax(game_tree, tang + 1, true, x, y, out _, out _);
-                    if (ndtn > value)
+                    int nx = i / 10; // tính tọa độ x
+                    int ny = i % 10; // tính tọa độ y
+                    int ndht = diemtungnuoc(nx,ny,2); // tính điểm nước đi của đối thủ
+                    int value = minimax(game_tree, tang + 1, true, nx, ny, alpha, beta, out _, out _);   // gọi đệ quy
+                    int toi_uu = Math.Min(value, ndht);
+                    if (ndtn_dt > toi_uu)
                     {
-                        ndtn = value;
-                        tdi = x;
-                        tdj = y;
+                        ndtn_dt = toi_uu; // cập nhật giá trị nhỏ nhất
+                        tdi = nx;
+                        tdj = ny; // cập nhật tọa độ i,j
                     }
+                    beta = Math.Min(beta, ndtn_dt);
+                    if (beta <= alpha) // cắt tỉa nhánh
+                        break;
                 }
-                return ndtn;
+                return ndtn_dt; // trả về giá trị nhỏ nhất kèm tọa độ
             }
         }
     }
--- Checkers_flag/Models/checkgame.cs
+++ Checkers_flag/Models/checkgame.cs
@@ -17,34 +17,53 @@
                 for (int j = 0; j < m; j++)
                     a[i, j] = board[i, j];
         }
-        public bool ktr(int i, int j, int giatri)
+        public bool ktr(int i, int j, int nuocdi)
         {
-            if (a[i, j] != giatri) return false;
+            if (a[i, j] != nuocdi) return false;
             // kiểm tra hàng chéo phải xuống trái 
-            int dem = demchuoi(i, j, 1, -1, giatri) + demchuoi(i, j, -1, 1, giatri) - 1;
+            int dem = demchuoi(i, j, 1, -1, nuocdi) + demchuoi(i, j, -1, 1, nuocdi) - 1;
             if (dem >= 5) return true;
             // kiểm tra hàng chéo trái xuống phải 
-            dem = demchuoi(i, j, 1, 1, giatri) + demchuoi(i, j, -1, -1, giatri) - 1;
+            dem = demchuoi(i, j, 1, 1, nuocdi) + demchuoi(i, j, -1, -1, nuocdi) - 1;
             if (dem >= 5) return true;
             // kiểm tra hàng ngang
-            dem = demchuoi(i, j, 0, 1, giatri) + demchuoi(i, j, 0, -1, giatri) - 1;
+            dem = demchuoi(i, j, 0, 1, nuocdi) + demchuoi(i, j, 0, -1, nuocdi) - 1;
             if (dem >= 5) return true;
             // kiểm tra hàng dọc
-            dem = demchuoi(i, j, 1, 0, giatri) + demchuoi(i, j, -1, 0, giatri) - 1;
+            dem = demchuoi(i, j, 1, 0, nuocdi) + demchuoi(i, j, -1, 0, nuocdi) - 1;
             if (dem >= 5) return true;
             return false;
         }
-        public int demchuoi(int i, int j, int x, int y, int giatri)
+        public int demchuoi(int i, int j, int x, int y, int nuocdi)
         // x, y là hướng di chuyển trong ma trận
         {
             int dem = 0;
-            while (i >= 0 && i < 10 && j >= 0 && j < 10 && a[i, j] == giatri)
+            while (i >= 0 && i < 10 && j >= 0 && j < 10 && a[i, j] == nuocdi)
             {
                 dem++;
                 i += x;
                 j += y;
             }
             return dem;
+        }
+       
+        public int diemtungnuoc(int i, int j, int nuocdi)
+        {
+            int max = 0;
+            int dem;
+            // kiểm tra hàng chéo phải xuống trái 
+            dem = demchuoi(i, j, 1, -1, nuocdi) + demchuoi(i, j, -1, 1, nuocdi) - 1;
+            if (dem > max) max = dem;
+            // kiểm tra hàng chéo trái xuống phải 
+            dem = demchuoi(i, j, 1, 1, nuocdi) + demchuoi(i, j, -1, -1, nuocdi) - 1;
+            if (dem > max) max = dem;
+            // kiểm tra hàng ngang
+            dem = demchuoi(i, j, 0, 1, nuocdi) + demchuoi(i, j, 0, -1, nuocdi) - 1;
+            if (dem > max) max = dem;
+            // kiểm tra hàng dọc
+            dem = demchuoi(i, j, 1, 0, nuocdi) + demchuoi(i, j, -1, 0, nuocdi) - 1;
+            if (dem > max) max = dem;
+            return max;
         }
     }
 }
